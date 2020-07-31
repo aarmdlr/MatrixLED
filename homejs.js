7c46bbff0e9b24bf0e4f89541ad8f85498780ddb
aarmdlr <64794996+aarmdlr@users.noreply.github.com>
2020-07-31 12:48:43 +0200

    Add files via upload

--- homejs.js
+++ homejs.js
@@ -69,6 +69,7 @@ function setSelectedOptCountry() {
   //document.getElementById("select_COUNTRY_CODE").value = optCountryVal;
   alert(tempIndex);
   x.selectedIndex=tempIndex;
+  alert(optCountryVal);
 
 }
 
