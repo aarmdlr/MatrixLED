77da0f8f9026e978823ff9f430ddff77dd9b17f3
aarmdlr <64794996+aarmdlr@users.noreply.github.com>
2020-07-31 11:33:32 +0200

    Add files via upload

--- homejs.js
+++ homejs.js
@@ -16,8 +16,7 @@
     }
 })();*/
 
-
-(function () {
+/*(function () {
   document.getElementById("btnTime").addEventListener('click', makeRequest);//attaching click event for button
   function makeRequest() {
     var httpRequest = new XMLHttpRequest();// Initiatlization of XMLHttpRequest
@@ -25,4 +24,15 @@
     httpRequest.send();
     console.log(httpRequest.responseText);
   }
-})();
+})();*/
+
+function loadDoc() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("select_COUNTRY_CODE").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "https://aarmdlr.github.io/MatrixLED/country_codes.html", true);
+  xhttp.send();
+}
