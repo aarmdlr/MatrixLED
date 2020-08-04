2fa75367d18ff5fd85744ae632ec113c1f42a918
aarmdlr <64794996+aarmdlr@users.noreply.github.com>
2020-08-04 17:07:57 +0200

    Add files via upload

--- d-pad.js
+++ d-pad.js
@@ -1,99 +1,109 @@
 (function () {
+    var hostNameSTR=document.getElementById("input_deviceNameSTR_H").value;
     document.getElementById("btnUP").addEventListener('click', makeRequest);//attaching click event for button
     function makeRequest() {
       navigator.vibrate(100);
         var httpRequest = new XMLHttpRequest();// Initiatlization of XMLHttpRequest
-        httpRequest.open('GET', 'http://matrixled.lan/up'); // service call
+        httpRequest.open('GET', 'http://'+hostNameSTR+'.lan/up'); // service call
         httpRequest.send();
     }
 })();
 
 (function () {
+    var hostNameSTR=document.getElementById("input_deviceNameSTR_H").value;
     document.getElementById("btnDOWN").addEventListener('click', makeRequest);//attaching click event for button
     function makeRequest() {
       navigator.vibrate(100);
         var httpRequest = new XMLHttpRequest();// Initiatlization of XMLHttpRequest
-        httpRequest.open('GET', 'http://matrixled.lan/down'); // service call
+        httpRequest.open('GET', 'http://'+hostNameSTR+'.lan/down'); // service call
         httpRequest.send();
     }
 })();
 
 (function () {
+    var hostNameSTR=document.getElementById("input_deviceNameSTR_H").value;
     document.getElementById("btnLEFT").addEventListener('click', makeRequest);//attaching click event for button
     function makeRequest() {
       navigator.vibrate(100);
         var httpRequest = new XMLHttpRequest();// Initiatlization of XMLHttpRequest
-        httpRequest.open('GET', 'http://matrixled.lan/left'); // service call
+        httpRequest.open('GET', 'http://'+hostNameSTR+'.lan/left'); // service call
         httpRequest.send();
     }
 })();
 
 (function () {
+    var hostNameSTR=document.getElementById("input_deviceNameSTR_H").value;
     document.getElementById("btnRIGHT").addEventListener('click', makeRequest);//attaching click event for button
     function makeRequest() {
       navigator.vibrate(100);
         var httpRequest = new XMLHttpRequest();// Initiatlization of XMLHttpRequest
-        httpRequest.open('GET', 'http://matrixled.lan/right'); // service call
+        httpRequest.open('GET', 'http://'+hostNameSTR+'.lan/right'); // service call
         httpRequest.send();
     }
 })();
 
 (function () {
+    var hostNameSTR=document.getElementById("input_deviceNameSTR_H").value;
     document.getElementById("btnA").addEventListener('click', makeRequest);//attaching click event for button
     function makeRequest() {
       navigator.vibrate(100);
         var httpRequest = new XMLHttpRequest();// Initiatlization of XMLHttpRequest
-        httpRequest.open('GET', 'http://matrixled.lan/btna'); // service call
+        httpRequest.open('GET', 'http://'+hostNameSTR+'.lan/btna'); // service call
         httpRequest.send();
     }
 })();
 
 (function () {
+    var hostNameSTR=document.getElementById("input_deviceNameSTR_H").value;
     document.getElementById("btnB").addEventListener('click', makeRequest);//attaching click event for button
     function makeRequest() {
       navigator.vibrate(100);
         var httpRequest = new XMLHttpRequest();// Initiatlization of XMLHttpRequest
-        httpRequest.open('GET', 'http://matrixled.lan/btnb'); // service call
+        httpRequest.open('GET', 'http://'+hostNameSTR+'.lan/btnb'); // service call
         httpRequest.send();
     }
 })();
 
 (function () {
+    var hostNameSTR=document.getElementById("input_deviceNameSTR_H").value;
     document.getElementById("btnX").addEventListener('click', makeRequest);//attaching click event for button
     function makeRequest() {
       navigator.vibrate(100);
         var httpRequest = new XMLHttpRequest();// Initiatlization of XMLHttpRequest
-        httpRequest.open('GET', 'http://matrixled.lan/btnx'); // service call
+        httpRequest.open('GET', 'http://'+hostNameSTR+'.lan/btnx'); // service call
         httpRequest.send();
     }
 })();
 
 (function () {
+    var hostNameSTR=document.getElementById("input_deviceNameSTR_H").value;
     document.getElementById("btnY").addEventListener('click', makeRequest);//attaching click event for button
     function makeRequest() {
       navigator.vibrate(100);
         var httpRequest = new XMLHttpRequest();// Initiatlization of XMLHttpRequest
-        httpRequest.open('GET', 'http://matrixled.lan/btny'); // service call
+        httpRequest.open('GET', 'http://'+hostNameSTR+'.lan/btny'); // service call
         httpRequest.send();
     }
 })();
 
 (function () {
+    var hostNameSTR=document.getElementById("input_deviceNameSTR_H").value;
     document.getElementById("btnSTART").addEventListener('click', makeRequest);//attaching click event for button
     function makeRequest() {
       navigator.vibrate(100);
         var httpRequest = new XMLHttpRequest();// Initiatlization of XMLHttpRequest
-        httpRequest.open('GET', 'http://matrixled.lan/start'); // service call
+        httpRequest.open('GET', 'http://'+hostNameSTR+'.lan/start'); // service call
         httpRequest.send();
     }
 })();
 
 (function () {
+    var hostNameSTR=document.getElementById("input_deviceNameSTR_H").value;
     document.getElementById("btnSELECT").addEventListener('click', makeRequest);//attaching click event for button
     function makeRequest() {
         navigator.vibrate(100);
         var httpRequest = new XMLHttpRequest();// Initiatlization of XMLHttpRequest
-        httpRequest.open('GET', 'http://matrixled.lan/select'); // service call
+        httpRequest.open('GET', 'http://'+hostNameSTR+'.lan/select'); // service call
         httpRequest.send();
     }
 })();
--- d-pad_Snake.js
+++ d-pad_Snake.js
@@ -1,99 +1,109 @@
 (function () {
+    var hostNameSTR=document.getElementById("input_deviceNameSTR_H").value;
     document.getElementById("btnUP").addEventListener('click', makeRequest);//attaching click event for button
     function makeRequest() {
       navigator.vibrate(100);
         var httpRequest = new XMLHttpRequest();// Initiatlization of XMLHttpRequest
-        httpRequest.open('GET', 'http://matrixled.lan/snakeup'); // service call
+        httpRequest.open('GET', 'http://'+hostNameSTR+'.lan/snakeup'); // service call
         httpRequest.send();
     }
 })();
 
 (function () {
+    var hostNameSTR=document.getElementById("input_deviceNameSTR_H").value;
     document.getElementById("btnDOWN").addEventListener('click', makeRequest);//attaching click event for button
     function makeRequest() {
       navigator.vibrate(100);
         var httpRequest = new XMLHttpRequest();// Initiatlization of XMLHttpRequest
-        httpRequest.open('GET', 'http://matrixled.lan/snakedown'); // service call
+        httpRequest.open('GET', 'http://'+hostNameSTR+'.lan/snakedown'); // service call
         httpRequest.send();
     }
 })();
 
 (function () {
+    var hostNameSTR=document.getElementById("input_deviceNameSTR_H").value;
     document.getElementById("btnLEFT").addEventListener('click', makeRequest);//attaching click event for button
     function makeRequest() {
       navigator.vibrate(100);
         var httpRequest = new XMLHttpRequest();// Initiatlization of XMLHttpRequest
-        httpRequest.open('GET', 'http://matrixled.lan/snakeleft'); // service call
+        httpRequest.open('GET', 'http://'+hostNameSTR+'.lan/snakeleft'); // service call
         httpRequest.send();
     }
 })();
 
 (function () {
+    var hostNameSTR=document.getElementById("input_deviceNameSTR_H").value;
     document.getElementById("btnRIGHT").addEventListener('click', makeRequest);//attaching click event for button
     function makeRequest() {
       navigator.vibrate(100);
         var httpRequest = new XMLHttpRequest();// Initiatlization of XMLHttpRequest
-        httpRequest.open('GET', 'http://matrixled.lan/snakeright'); // service call
+        httpRequest.open('GET', 'http://'+hostNameSTR+'.lan/snakeright'); // service call
         httpRequest.send();
     }
 })();
 
 (function () {
+    var hostNameSTR=document.getElementById("input_deviceNameSTR_H").value;
     document.getElementById("btnA").addEventListener('click', makeRequest);//attaching click event for button
     function makeRequest() {
       navigator.vibrate(100);
         var httpRequest = new XMLHttpRequest();// Initiatlization of XMLHttpRequest
-        httpRequest.open('GET', 'http://matrixled.lan/snakebtna'); // service call
+        httpRequest.open('GET', 'http://'+hostNameSTR+'.lan/snakebtna'); // service call
         httpRequest.send();
     }
 })();
 
 (function () {
+    var hostNameSTR=document.getElementById("input_deviceNameSTR_H").value;
     document.getElementById("btnB").addEventListener('click', makeRequest);//attaching click event for button
     function makeRequest() {
       navigator.vibrate(100);
         var httpRequest = new XMLHttpRequest();// Initiatlization of XMLHttpRequest
-        httpRequest.open('GET', 'http://matrixled.lan/snakebtnb'); // service call
+        httpRequest.open('GET', 'http://'+hostNameSTR+'.lan/snakebtnb'); // service call
         httpRequest.send();
     }
 })();
 
 (function () {
+    var hostNameSTR=document.getElementById("input_deviceNameSTR_H").value;
     document.getElementById("btnX").addEventListener('click', makeRequest);//attaching click event for button
     function makeRequest() {
       navigator.vibrate(100);
         var httpRequest = new XMLHttpRequest();// Initiatlization of XMLHttpRequest
-        httpRequest.open('GET', 'http://matrixled.lan/snakebtnx'); // service call
+        httpRequest.open('GET', 'http://'+hostNameSTR+'.lan/snakebtnx'); // service call
         httpRequest.send();
     }
 })();
 
 (function () {
+    var hostNameSTR=document.getElementById("input_deviceNameSTR_H").value;
     document.getElementById("btnY").addEventListener('click', makeRequest);//attaching click event for button
     function makeRequest() {
       navigator.vibrate(100);
         var httpRequest = new XMLHttpRequest();// Initiatlization of XMLHttpRequest
-        httpRequest.open('GET', 'http://matrixled.lan/snakebtny'); // service call
+        httpRequest.open('GET', 'http://'+hostNameSTR+'.lan/snakebtny'); // service call
         httpRequest.send();
     }
 })();
 
 (function () {
+    var hostNameSTR=document.getElementById("input_deviceNameSTR_H").value;
     document.getElementById("btnSTART").addEventListener('click', makeRequest);//attaching click event for button
     function makeRequest() {
       navigator.vibrate(100);
         var httpRequest = new XMLHttpRequest();// Initiatlization of XMLHttpRequest
-        httpRequest.open('GET', 'http://matrixled.lan/snakestart'); // service call
+        httpRequest.open('GET', 'http://'+hostNameSTR+'.lan/snakestart'); // service call
         httpRequest.send();
     }
 })();
 
 (function () {
+    var hostNameSTR=document.getElementById("input_deviceNameSTR_H").value;
     document.getElementById("btnSELECT").addEventListener('click', makeRequest);//attaching click event for button
     function makeRequest() {
         navigator.vibrate(100);
         var httpRequest = new XMLHttpRequest();// Initiatlization of XMLHttpRequest
-        httpRequest.open('GET', 'http://matrixled.lan/snakeselect'); // service call
+        httpRequest.open('GET', 'http://'+hostNameSTR+'.lan/snakeselect'); // service call
         httpRequest.send();
     }
 })();
