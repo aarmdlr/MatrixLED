45aed0c96ac358f726fae80e92572643a39da441
aarmdlr <64794996+aarmdlr@users.noreply.github.com>
2021-01-28 15:38:09 +0100

    Add files via upload

--- homejs_Blinds.js
+++ homejs_Blinds.js
@@ -541,6 +541,12 @@ webpage += "<div id='wrapper'>";
     webpage += "</div>";
   webpage += "</div>";
 
+  webpage += "<script>";
+webpage += "$(document).ready(function(){$(document).on('input', '#inputDistanceCML', function() {$('#CM_STEPPER_MAX_STEPS_L').val($(this).val()*1427);});";
+webpage += "$(document).on('input', '#inputDistanceCMC', function() {$('#CM_STEPPER_MAX_STEPS_C').val($(this).val()*1427);});";
+webpage += "$(document).on('input', '#inputDistanceCMR', function() {$('#CM_STEPPER_MAX_STEPS_R').val($(this).val()*1427);});";
+webpage += "$('#sidebarToggleTop').click(function() {$('#accordionSidebar').toggle();});});";
+webpage += "</script>";
 
 
 
