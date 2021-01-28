4b3e9bc499fec921ba5124a3d29e62b6ac7fde74
aarmdlr <64794996+aarmdlr@users.noreply.github.com>
2021-01-28 14:00:43 +0100

    Add files via upload

--- homejs_Blinds.js
+++ homejs_Blinds.js
@@ -723,7 +723,7 @@ function sendRANGE_async_update(identifier){
     httpRequest.onreadystatechange = function (aEvt) {
       if (httpRequest.readyState == 4) {
          if(httpRequest.status == 302){
-          //console.log(httpRequest.responseText);
+          console.log(httpRequest.responseText);
           var resultPerRangeGetted=httpRequest.responseText.split(",");
 
           STR_PER_STEPPER_TARGET_L = resultPerRangeGetted[0];
